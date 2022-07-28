589981db1a4a11bcff8f1c42dbaea0c9fb43b4b8
Keerthi Vasan S A <sakeerthi23@gmail.com>
2022-07-28 15:07:35 +0530

    changed floating btn position, added end card and footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,22 @@
+<div class="bg-black text-white py-10 text-center text-sm">
+	<div class="flex flex-col sm:flex-row justify-center gap-32 items-center my-20">
+		<img src="vilo_lg.png" width="100px" alt="vilo logo" />
+		<hr class="h-32 bg-gray-200 hidden sm:block" style="width:1px" />
+		<div class="flex gap-12 flex-wrap">
+			<div class="flex flex-col gap-2 mx-12">
+				<h3 class="text-xl font-bold">Me</h3>
+				<a href='#m'>My Spaces</a>
+				<a href='#m'>Account</a>
+				<a href='#m'>Settings</a>
+			</div>
+
+			<div class="flex flex-col gap-2 mx-12">
+				<h3 class="text-xl font-bold">Legal</h3>
+				<span>Terms</span>
+				<span>Privacy Policy</span>
+				<span>Data Usage</span>
+			</div>
+		</div>
+	</div>
+	<small class="w-full text-center">Copyright 2022, Keerthi Vasan S A</small>
+</div>
